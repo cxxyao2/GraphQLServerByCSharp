edf184729f1040c545f4c30d271054ef42dc24d2
jennifer <cxxyao2@gmail.com>
2024-04-03 21:03:12 -0300

    add AddTypeExtension to program.cs

---
+++ GraphQLDemo2.API/Schema/Queries/CourseQuery.cs
@@ -0,0 +1,104 @@
+﻿using GraphQLDemo2.API.Data;
+using GraphQLDemo2.API.Entities;
+using GraphQLDemo2.API.Schema.Filters;
+using GraphQLDemo2.API.Schema.Sorters;
+using GraphQLDemo2.API.Services.Courses;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraphQLDemo2.API.Schema.Queries
+{
+    [ExtendObjectType(typeof(Query))]
+    public class CourseQuery
+    {
+
+        private readonly CoursesRepository _coursesRepository;
+
+        public CourseQuery(CoursesRepository coursesRepository)
+        {
+            _coursesRepository = coursesRepository;
+        }
+
+        public async Task<IEnumerable<CourseQueryType>> GetAllCourses()
+        {
+            var courses = await _coursesRepository.GetAllCourses();
+
+            return courses.Select(c => new CourseQueryType()
+            {
+                Id = c.Id,
+                Name = c.Name,
+                Subject = c.Subject,
+                InstructorId = c.InstructorId,
+
+
+            });
+
+        }
+
+        [UsePaging(IncludeTotalCount = true, DefaultPageSize = 5)]
+        public async Task<IEnumerable<CourseQueryType>> GetCourses()
+        {
+            var courses = await _coursesRepository.GetAllCourses();
+
+            return courses.Select(c => new CourseQueryType()
+            {
+                Id = c.Id,
+                Name = c.Name,
+                Subject = c.Subject,
+                InstructorId = c.InstructorId,
+
+
+            });
+
+        }
+
+
+        [UseOffsetPaging(IncludeTotalCount = true, DefaultPageSize = 5)]
+        public async Task<IEnumerable<CourseQueryType>> GetOffsetCourses()
+        {
+            var courses = await _coursesRepository.GetAllCourses();
+
+            return courses.Select(c => new CourseQueryType()
+            {
+                Id = c.Id,
+                Name = c.Name,
+                Subject = c.Subject,
+                InstructorId = c.InstructorId,
+
+
+            });
+
+        }
+
+
+        [UseDbContext(typeof(DataContext))]
+        [UsePaging(IncludeTotalCount = true, DefaultPageSize = 5)]
+        [UseProjection]
+        [UseFiltering(typeof(CourseFilterType))]
+        [UseSorting(typeof(CourseSortType))]
+        public IQueryable<CourseQueryType> GetPaginatedCourses([ScopedService] DataContext context)
+        {
+            return context.Courses.Select(c => new CourseQueryType()
+            {
+                Id = c.Id,
+                Name = c.Name,
+                Subject = c.Subject,
+                InstructorId = c.InstructorId,
+
+            });
+
+        }
+
+       
+        public async Task<Course> GetCourseById(int id)
+        {
+            await Task.Delay(1000);
+
+            Course course = new Course();
+            course.Id = id;
+            return course;
+        }
+
+
+       
+    }
+}
--- GraphQLDemo2.API/Schema/Queries/Query.cs
+++ GraphQLDemo2.API/Schema/Queries/Query.cs
@@ -9,87 +9,10 @@ namespace GraphQLDemo2.API.Schema.Queries
 {
     public class Query
     {
-
-        private readonly CoursesRepository _coursesRepository;
-
-        public Query(CoursesRepository coursesRepository)
-        {
-            _coursesRepository = coursesRepository;
-        }
-
-        public async Task<IEnumerable<CourseQueryType>> GetAllCourses()
-        {
-            var courses = await _coursesRepository.GetAllCourses();
-
-            return courses.Select(c => new CourseQueryType()
-            {
-                Id = c.Id,
-                Name = c.Name,
-                Subject = c.Subject,
-                InstructorId = c.InstructorId,
-
-
-            });
-
-        }
-
-        [UsePaging(IncludeTotalCount = true, DefaultPageSize = 5)]
-        public async Task<IEnumerable<CourseQueryType>> GetCourses()
-        {
-            var courses = await _coursesRepository.GetAllCourses();
-
-            return courses.Select(c => new CourseQueryType()
-            {
-                Id = c.Id,
-                Name = c.Name,
-                Subject = c.Subject,
-                InstructorId = c.InstructorId,
-
-
-            });
-
-        }
-
-
-        [UseOffsetPaging(IncludeTotalCount = true, DefaultPageSize = 5)]
-        public async Task<IEnumerable<CourseQueryType>> GetOffsetCourses()
-        {
-            var courses = await _coursesRepository.GetAllCourses();
-
-            return courses.Select(c => new CourseQueryType()
-            {
-                Id = c.Id,
-                Name = c.Name,
-                Subject = c.Subject,
-                InstructorId = c.InstructorId,
-
-
-            });
-
-        }
-
-
-        [UseDbContext(typeof(DataContext))]
-        [UsePaging(IncludeTotalCount = true, DefaultPageSize = 5)]
-        [UseProjection]
-        [UseFiltering(typeof(CourseFilterType))]
-        [UseSorting(typeof(CourseSortType))]
-        public IQueryable<CourseQueryType> GetPaginatedCourses([ScopedService] DataContext context)
-        {
-            return context.Courses.Select(c => new CourseQueryType()
-            {
-                Id = c.Id,
-                Name = c.Name,
-                Subject = c.Subject,
-                InstructorId = c.InstructorId,
-
-            });
-
-        }
-
         [UseDbContext(typeof(DataContext))]
         public async Task<IEnumerable<ISearchResultType>> Search(string term, [ScopedService] DataContext context)
         {
+
             IEnumerable<CourseQueryType> courses = await context.Courses
                 .Where(c => c.Name.Contains(term))
                 .Select(c => new CourseQueryType()
@@ -118,17 +41,6 @@ namespace GraphQLDemo2.API.Schema.Queries
                 .Concat(instructors);
         
         }
-       
-
-        public async Task<Course> GetCourseById(int id)
-        {
-            await Task.Delay(1000);
-
-            Course course = new Course();
-            course.Id = id;
-            return course;
-        }
-
 
         [GraphQLDeprecated("This query is deprecated.")]
         public string Instructions => "Smash tha like button and subscribe to Singletons";
